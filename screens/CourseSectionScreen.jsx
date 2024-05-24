d97d19db61ab0648a7cea6ca19d6ff28d71b8fdc
supun-ganegoda <supung@syntaxgenie.com>
2024-05-24 15:48:40 +0530

    Video and web view content added

--- screens/CourseSectionScreen.jsx
+++ screens/CourseSectionScreen.jsx
@@ -5,11 +5,14 @@ import {
   StyleSheet,
   Dimensions,
   TouchableOpacity,
+  ScrollView,
 } from "react-native";
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, useCallback } from "react";
 import { useRoute } from "@react-navigation/native";
 import { Colors } from "../constants/Colors";
 import { useNavigation } from "@react-navigation/native";
+import YoutubePlayer from "react-native-youtube-iframe";
+import { WebView } from "react-native-webview";
 
 export default function CourseSectionScreen() {
   const listRef = useRef(null);
@@ -17,7 +20,14 @@ export default function CourseSectionScreen() {
   const navigation = useNavigation();
   const [currentIndex, setCurrentIndex] = useState(1);
   const [btnText, setBtnText] = useState("Next");
+  const [playing, setPlaying] = useState(false);
   const pages = topic.content.length;
+
+  const onStateChange = useCallback((state) => {
+    if (state === "ended") {
+      setPlaying(false);
+    }
+  }, []);
 
   const handleScroll = () => {
     if (currentIndex === pages - 1) {
@@ -34,35 +44,62 @@ export default function CourseSectionScreen() {
 
   return (
     <View style={styles.container}>
-      <FlatList
-        ref={listRef}
-        data={topic?.content}
-        horizontal={true}
-        pagingEnabled={true}
-        showsHorizontalScrollIndicator={false}
-        scrollEnabled={false}
-        renderItem={({ item }) => (
-          <View style={styles.sectionView}>
-            <Text style={styles.caption}>{item.caption}</Text>
-            <Text style={styles.description}>{item.description}</Text>
-          </View>
-        )}
-      />
-      <TouchableOpacity style={styles.nextBtn} onPress={handleScroll}>
-        <Text style={styles.btnText}>{btnText}</Text>
-      </TouchableOpacity>
+      <ScrollView
+        style={styles.scrollView}
+        contentContainerStyle={{ flexGrow: 1 }}
+      >
+        <FlatList
+          ref={listRef}
+          data={topic?.content}
+          horizontal={true}
+          pagingEnabled={true}
+          showsHorizontalScrollIndicator={false}
+          scrollEnabled={false}
+          renderItem={({ item }) => (
+            <View style={styles.sectionView}>
+              <Text style={styles.caption}>{item.caption}</Text>
+              {item.utube && (
+                <View style={styles.utube}>
+                  <YoutubePlayer
+                    height={Dimensions.get("window").height * 0.25}
+                    play={playing}
+                    videoId={item.utube}
+                    onChangeState={onStateChange}
+                  />
+                </View>
+              )}
+              {item.web && (
+                <View style={styles.web}>
+                  <WebView source={{ uri: item.web }} />
+                </View>
+              )}
+              <Text style={styles.section}>Description</Text>
+              <Text style={styles.description}>{item.description}</Text>
+            </View>
+          )}
+        />
+      </ScrollView>
+      <View>
+        <TouchableOpacity style={styles.nextBtn} onPress={handleScroll}>
+          <Text style={styles.btnText}>{btnText}</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
     padding: 10,
+  },
+  scrollView: {
+    marginBottom: 10,
   },
   sectionView: {
     width: Dimensions.get("window").width * 0.95,
-    height: Dimensions.get("window").height * 0.88,
-    marginEnd: 5,
+    height: "auto",
+    paddingBottom: 15,
     backgroundColor: Colors.light.white,
   },
   caption: {
@@ -74,8 +111,6 @@ const styles = StyleSheet.create({
   },
   nextBtn: {
     display: "flex",
-    position: "relative",
-    top: 10,
     padding: 10,
     borderRadius: 10,
     backgroundColor: Colors.light.background,
@@ -83,5 +118,21 @@ const styles = StyleSheet.create({
   },
   btnText: {
     fontSize: 18,
+  },
+  section: {
+    fontSize: 20,
+    padding: 5,
+    fontWeight: "400",
+  },
+  utube: {
+    padding: 5,
+    marginVertical: 10,
+  },
+  web: {
+    borderWidth: 1,
+    borderColor: "#000",
+    margin: 5,
+    width: "auto",
+    height: Dimensions.get("window").height * 0.75,
   },
 });
